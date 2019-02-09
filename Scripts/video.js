dcb78d8be8159917fcf54fbec15c2d7eb51fc83d
Alejandro Garzón <cagg40@hotmail.com>
2019-02-09 16:44:49 -0500

    Hover and focus added to the buttons

--- Scripts/video.js
+++ Scripts/video.js
@@ -3,3 +3,17 @@ function changeVideo(link){
     document.getElementById("SourceVideo").src = link;
     vid.load();
 }
+
+function move() {
+    var elem = document.getElementById("Progress");   
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+      }
+    }
+  }
