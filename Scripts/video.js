fc84b9e8f80610d176e37c78c6a664102d376353
Momo <43181268+Skauron@users.noreply.github.com>
2024-07-09 15:17:27 -0500

    Update portafolio

--- Scripts/video.js
+++ Scripts/video.js
@@ -1,19 +1,30 @@
-function changeVideo(link){
-    var vid = document.getElementById("video-portafolio");
-    document.getElementById("SourceVideo").src = link;
-    vid.load();
+function changeVideo(link) {
+  var vid = document.getElementById("video-portafolio");
+  document.getElementById("SourceVideo").src = link;
+  vid.load();
 }
 
 function move() {
-    var elem = document.getElementById("Progress");   
-    var width = 1;
-    var id = setInterval(frame, 10);
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-      } else {
-        width++; 
-        elem.style.width = width + '%'; 
-      }
+  var elem = document.getElementById("Progress");
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++;
+      elem.style.width = width + '%';
     }
   }
+}
+
+const OpenWithDelay = function (milisegundos, url) {
+  setTimeout(function () {
+    window.open(url);
+  }, milisegundos);
+}
+
+function OpenNewTabPortafolio(url, fileName) {
+  document.getElementById("SourceImg").src = fileName;
+  OpenWithDelay(1000, url);
+}
